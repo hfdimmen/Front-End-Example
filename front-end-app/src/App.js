719425d86e45d3ff195538064177575a411b0596
Hans Fredrik Dimmen <hfdimmen@gmail.com>
2020-12-21 18:53:39 +0100

    Add cat svg and prepare for API integration

--- front-end-app/src/App.js
+++ front-end-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import cat from './cat.svg';
 import './App.css';
 import styled from "styled-components";
 
@@ -6,19 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+          <img src={cat} className="App-cat" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to this cat-fact frontend test
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        <p>Cat facts coming soon!</p>
+      </body>    
     </div>
   );
 }
