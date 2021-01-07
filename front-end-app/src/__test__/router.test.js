3abaa03cd48ad149ab94e4b857b2da7acb3091aa
Hans Fredrik Dimmen <hfdimmen@gmail.com>
2021-01-07 14:18:08 +0100

    Add unit tests for router including the PageNotFound scenario

---
+++ front-end-app/src/__test__/router.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from 'react-router';
+
+import App from '../App';
+import Home from '../pages/home/Home';
+import Header from '../header/Header';
+import Router from '../router';
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('../header/Header', () => () => <div>HeaderMock</div>);
+jest.mock('../pages/home/Home', () => () => <div>HomeMock</div>);
+jest.mock('../pages/facts/Facts', () => () => <div>FactsMock</div>);
+jest.mock('../pages/pageNotFound/PageNotFound', () => () => <div>PageNotFoundMock</div>);
+
+afterEach(cleanup);
+describe("Tests for router", () => {
+    test("Should render page header and Home on default route", () => {
+
+        render(
+        <App />
+        );
+        
+        expect(screen.getByText("HeaderMock")).toBeInTheDocument();
+        expect(screen.getByText("HomeMock")).toBeInTheDocument();
+      
+    });
+
+    test("Should render page header and Home on default route", () => {
+
+        render(
+        <MemoryRouter initialEntries={['/facts']}>
+            <Router />
+        </MemoryRouter>
+        );
+        
+        expect(screen.getByText("FactsMock")).toBeInTheDocument();
+      
+    });
+
+    test("Should render page header and Home on default route", () => {
+        
+        render(
+        <MemoryRouter initialEntries={['/invalid']}>
+            <Router />
+        </MemoryRouter>
+        );
+        
+        expect(screen.getByText("PageNotFoundMock")).toBeInTheDocument();
+    });
+});
